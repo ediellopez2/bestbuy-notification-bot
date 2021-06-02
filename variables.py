7f1fd160b55291f7c3e71459a152e05f7cd65a49
Ediel <ediellopez2@gmail.com>
2021-06-02 16:15:07 -0700

    Variables for bot.

--- variables.py
+++ variables.py
@@ -1,3 +1,8 @@
-EMAIL_USER = 'your gmail email address goes here'
-EMAIL_RECEPIENT = 'recipient email address goes here'
-EMAIL_PASS = '16-digit password goes here'
+URL = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
+
+TWILIO_ACCOUNT_SID = ""
+TWILIO_AUTH_TOKEN = ""
+number_twilio = ""
+number_luis = ""
