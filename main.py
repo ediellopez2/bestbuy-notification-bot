b0a7ed0a993c4411a84c018f32bc1adb6f6d3584
Ediel <ediellopez2@gmail.com>
2021-06-02 16:13:22 -0700

    New and improved script.
    Improved error handling.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import variables        # Stores my Google Email & Password
+import variables                          # Stores variables
 import requests                           # Web Scraping
 from bs4 import BeautifulSoup             # Web Scraping
-import smtplib                            # Send Email
-from email.message import EmailMessage    # Send Email
+from twilio.rest import Client            # Twilio
 from datetime import datetime             # Display Time
 import time                               # Go To Sleep
 
@@ -10,58 +9,60 @@ import time                               # Go To Sleep
 # 1. Determine the User-Agent
 # http://httpbin.org/get
 
-def sendEmail(subject,message):
-    msg = EmailMessage()
-    msg['Subject'] = subject
-    msg['From'] = variables.EMAIL_USER
-    msg['To'] = variables.EMAIL_RECEPIENT
-    msg.set_content(message)
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(variables.EMAIL_USER, variables.EMAIL_PASS)
-        smtp.send_message(msg)
+def send_sms(recipient, message):
+    client = Client(variables.TWILIO_ACCOUNT_SID, variables.TWILIO_AUTH_TOKEN)
+
+    client.messages.create(
+        body=message,
+        from_=variables.number_twilio,
+        to=recipient
+    )
+    return
+
 
 if __name__ == "__main__":
-    while (True):
+    while True:
         try:
-            #                             Web Scraping
-            # =======================================================================
-            URL = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402'
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
+            page = requests.get(variables.URL, headers=variables.HEADERS)
 
-            page = requests.get(URL, headers=headers)
-            soup = BeautifulSoup(page.text, 'html.parser')
+            # 2xx successful – the request was successfully received, understood, and accepted
+            if page.status_code == 200:
+                soup = BeautifulSoup(page.text, 'html.parser')
 
-            # Uncomment this to see all of the source code from the website. IT IS SUPER LONG!!
-            # print(soup.prettify())
+                # Uncomment this to see all of the source code from the website. IT IS SUPER LONG!!
+                # print(soup.prettify())
 
-            # Get the name of the product.
-            product_title = soup.find('div', class_='sku-title')
-            # print(product_title.string)
+                # Get the name of the product.
+                product_title = soup.find('div', class_='sku-title')
+                # print(product_title.string)
 
-            # Get the status of the product (Add to Cart/Sold Out).
-            determine_status = soup.find('button', class_='add-to-cart-button')
-            # print(determine_status.string)
+                # Get the status of the product (Add to Cart/Sold Out).
+                determine_status = soup.find('button', class_='add-to-cart-button')
 
-            message = ""
-            if (determine_status.string == "Sold Out"):
-                message = datetime.now().strftime('%Y-%m-%d %H:%M:%S').__str__() + ": " + product_title.string + " is " + determine_status.string
-            else:
-                message = datetime.now().strftime('%Y-%m-%d %H:%M:%S').__str__() + ": " + "GREAT NEWS! " + product_title.string + " is In Stock!"
-                sendEmail('BestBuy Notification Bot', message)
+                message = ""
+                if determine_status.text == "Sold Out":
+                    message = datetime.now().strftime('%Y-%m-%d %H:%M:%S').__str__() + ": " + product_title.string + " is " + determine_status.string
+                else:
+                    message = datetime.now().strftime('%Y-%m-%d %H:%M:%S').__str__() + ": " + "GREAT NEWS! " + product_title.string + " is In Stock!"
+                    send_sms(variables.number_luis, message)
 
-            # Print to the console.
-            print(message)
-            time.sleep(30)
-            # =======================================================================
-        except Exception as e:
+                # Print to the console.
+                print(message)
+                time.sleep(30)
+        except requests.exceptions.ConnectionError as errc:
+            # In the event of a network problem (e.g. DNS failure, refused connection, etc),
+            # Requests will raise a ConnectionError exception.
+            print("OOPS!! Connection Error. Make sure you are connected to Internet. Technical Details given below.")
+            print(errc.__str__())
 
-            # Print to the console.
-            print("AN ERROR OCCURRED AT " + datetime.now().strftime('%Y-%m-%d %H:%M:%S').__str__() + "!\nHere is the specific error:\n\n")
-            print(e.__str__())
+            # Sleep for 1 minute and try to run the program again.
+            time.sleep(60)  # 1 minute
+            continue
+        except Exception as exc:
+            # This block will execute when an unexpected error that is unrelated to connection error occurs.
+            errorMessage = "Your bot crashed at " + datetime.now().strftime('%Y-%m-%d %H:%M:%S').__str__() + "!\n"
+            print(errorMessage + "Here is the specific error:\n" + exc.__str__())
 
-            # Create message and send the email.
-            message = "AN ERROR OCCURRED AT " + datetime.now().strftime('%Y-%m-%d %H:%M:%S').__str__() + "!\nHere is the specific error:\n\n" + e.__str__()
-            sendEmail('Problem: BestBuy Notification Bot', message)
+            send_sms(variables.number_luis, message)
             break
